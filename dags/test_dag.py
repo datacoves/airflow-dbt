5b3d723626b3bcd0e5cd118c6221f05138c2c52d
Leonardo Pelanda <ing.leonardo@pelanda.es>
2021-11-08 19:05:24 +0000

    Directorr Updated

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -3,8 +3,12 @@ from airflow_dbt.operators.dbt_operator import DbtBuildOperator
 
 from airflow.utils.dates import days_ago
 
-default_args = {"dir": "/root/dbt_project/air_dbt", "start_date": days_ago(0)}
-with DAG(dag_id="dbt", default_args=default_args, schedule_interval="@daily") as dag:
+default_args = {"dir": "/root/dbt_project", "start_date": days_ago(0)}
+with DAG(
+    dag_id="DbtBuildOperator_Test",
+    default_args=default_args,
+    schedule_interval="@daily",
+) as dag:
 
     dbt_build = DbtBuildOperator(
         task_id="dbt_buid",
